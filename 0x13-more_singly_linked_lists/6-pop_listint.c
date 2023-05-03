7c03a2525fe4ef63dc93aa19b8311a895c710747
TatendaDM <TatendaDM@github.com>
2023-05-02 19:46:01 -0700

    snow

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
  * pop_listint - node head of a linked list to be deleted
  * @head: first element in the linked list pointer
  *
- * Return: shows the data inside the elements that was deleted, 
+ * Return: shows the data of the elements that were deleted, 
  * or 0 if the list is empty
  */
 int pop_listint(listint_t **head)
